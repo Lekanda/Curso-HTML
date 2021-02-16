2a0af2168985e5ec864c163995c7e6e8d688bba1
lekanda <abernaolao@gmail.com>
2021-02-16 10:50:05 +0100

    98.Variables en JS.

---
+++ 4-JavaScript/js/01-variables.js
@@ -0,0 +1,28 @@
+//  VARIABLES
+
+var producto='Audifonos Gamer'; // Iniciar variables y asignar valor
+var disponible; // Inicia variable pero sin valor.
+
+// producto = true; // Reasignar el valor de la variable.
+disponible = true; // Asignar el valor de la disponible.
+
+// Iniciar muchas variables
+var producto1='Computadora',
+    disponible1= true,
+    categoria='Computadoras';
+
+
+// Las variables pueden tener numeros y simbolos pero no pueden empezar con ellos
+
+// var 1casa; // Incorrecto.
+// var -casa; // Incorrecto.
+var _casa; // Correcto.
+
+
+// Estilos para las variables.
+var nombre_casa; // Underscore
+var nombreCasa; // Camelcase. El mas usual
+var NombreCasa; // Pascal case. Solo para clases
+var nombrecasa; // Lower case. El normal o menos utilizado en JS.
+
+console.log(producto);
