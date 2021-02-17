038a44af27167dc579d3f21d4884668314987cab
lekanda <abernaolao@gmail.com>
2021-02-17 10:30:45 +0100

    102.Numeros y operadores.

---
+++ 4-JavaScript/js/05-NumerosYoperadores.js
@@ -0,0 +1,18 @@
+// NUMEROS
+
+// No hay float en JS. Los pone como number todos.
+const numero1 = '100'; // String
+const numero2 = 200; // Number
+const numero3 = 200.2; // Number
+const numero4 = .1020; // Number
+const numero5 = -5; // Number
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+
+// Igual con -(Resta) /(Division) *(Multiplicacion) %(Modulo)
+console.log(numero3 + numero2);
