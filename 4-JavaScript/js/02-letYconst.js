82140c78f0fba7eadd59c445a7f74f1d163d17f0
lekanda <abernaolao@gmail.com>
2021-02-17 09:37:48 +0100

    99.Let y Const.

---
+++ 4-JavaScript/js/02-letYconst.js
@@ -0,0 +1,28 @@
+// LET
+
+let producto='Audifonos Gamer'; // Iniciar let varibles y asignar valor
+let disponible; // Inicia leti variable pero sin valor.
+
+// producto = true; // Reasignar el valor de la variable.
+disponible = true; // Asignar el valor de la disponible.
+
+// Iniciar muchas variables
+let producto1='Computadora',
+    disponible1= true,
+    categoria='Computadoras';
+
+
+// Las variables pueden tener numeros y simbolos pero no pueden empezar con ellos
+
+// let 1casa; // Incorrecto.
+// let -casa; // Incorrecto.
+let _casa; // Correcto.
+
+
+// Estilos para las variables.
+let nombre_casa; // Underscore
+let nombreCasa; // Camelcase. El mas usual
+let NombreCasa; // Pascal case. Solo para clases
+let nombrecasa; // Lower case. El normal o menos utilizado en JS.
+
+console.log(producto);
---
+++ 4-JavaScript/js/03-const.js
@@ -0,0 +1,29 @@
+// CONST. No se puede reasignar el valor.
+// NO SE PUEDE INICIALIZAR UN CONST SIN VALOR !!!
+
+const producto='Audifonos Gamer'; // Iniciar const varibles y asignar valor
+// const disponible; // Inicia const variable pero sin valor. INCORRECTO, hace falta valor.
+
+// producto = true; // Reasignar el valor de la variable.
+// disponible = true; // No se puede asignar el valor de la disponible, es CONST.
+
+// Iniciar muchas variables
+const producto1='Computadora',
+    disponible1= true,
+    categoria='Computadoras';
+
+
+// Las variables const pueden tener numeros y simbolos pero no pueden empezar con ellos
+
+// const 1casa; // Incorrecto.
+// const -casa; // Incorrecto.
+// const _casa; // Incorrecto. Hace falta valor
+
+
+// Estilos para las variables.
+// const nombre_casa; // Underscore
+// const nombreCasa; // Camelcase. El mas usual
+// const NombreCasa; // Pascal case. Solo para clases
+// const nombrecasa; // Lower case. El normal o menos utilizado en JS.
+
+console.log(producto);
