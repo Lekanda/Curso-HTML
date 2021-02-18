934490c0a129fe23ee28c1c412a215aee608189e
lekanda <abernaolao@gmail.com>
2021-02-18 10:19:21 +0100

    103.Math.

---
+++ 4-JavaScript/js/06-math.js
@@ -0,0 +1,34 @@
+//  MATH
+
+let resultado;
+
+
+// PI
+resultado=Math.PI;
+// Redondear
+resultado=Math.round(2.2);
+// Redondeo hacia arriba
+resultado=Math.ceil(2.2);
+// Redondea hacia abajo.
+resultado=Math.floor(2.2);
+// Raiz cuadrada.
+resultado=Math.sqrt(9);
+// Convierte el numero en positivo
+resultado=Math.abs(-200);
+// Coge el mas pequeño
+resultado=Math.min(3,5,9,7,4,0);
+// Coge el mas grande
+resultado=Math.max(3,5,9,7,4,0);
+// Numero aleatorio
+resultado=Math.random();
+resultado=Math.floor(Math.random() * 30);
+
+
+
+
+
+
+
+
+
+console.log(resultado);
