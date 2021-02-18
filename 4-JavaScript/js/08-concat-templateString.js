d9d554314b622bf1da92312678169079d6b27c35
lekanda <abernaolao@gmail.com>
2021-02-18 10:52:15 +0100

    105.Concatenacion-Template Strings.

---
+++ 4-JavaScript/js/08-concat-templateString.js
@@ -0,0 +1,22 @@
+// Concatenacion y Template-String
+
+const nombre = 'Juan';
+const email = 'correo@correo.com';
+
+console.log(nombre);
+console.log(email);
+
+// Concatenar
+console.log(email + ' ' + nombre);
+console.log('Nombre cliente: ' + nombre + ' ' + email);// Metodo viejo.
+console.log('Nombre cliente: ' ,nombre, ' ' , email);// Metodo viejo.
+
+
+// Template Strings - String Literals.
+console.log(`Nombre cliente: ${nombre} Email: ${email}`);
+
+
+
+
+
+
