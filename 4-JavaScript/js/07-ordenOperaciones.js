c00c48cfd089ed712febbee85922cbbcfa0393ae
lekanda <abernaolao@gmail.com>
2021-02-18 10:38:54 +0100

    104.Orden de las operaciones.

---
+++ 4-JavaScript/js/07-ordenOperaciones.js
@@ -0,0 +1,33 @@
+// Orden de las Operaciones
+
+let resultado;
+// 30*2 y +20
+resultado=20+30*2;
+// 30+20 y *2
+resultado=(20+30)*2;
+
+resultado=(100+200+300) * 1.21;
+
+// Divisiones y multiplicaciones tienen mas peso que las + y -
+// console.log(resultado);
+/************************************************************/
+
+
+// Incrementos
+
+let puntos=10;
+puntos++
+
+// Igual con -
+// Imprime y lugo incrementa.
+console.log(puntos++);
+// Incrementa y luego imprime.
+console.log(++puntos);
+
+
+// Incrementa en 10
+puntos+=10;
+console.log(puntos);
+
+
+
