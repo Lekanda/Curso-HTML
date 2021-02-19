9717e4425090cbce396fea4980f2d412dd23b505
lekanda <abernaolao@gmail.com>
2021-02-19 10:29:17 +0100

    109.destructuring.

---
+++ 4-JavaScript/js/11-destructuring.js
@@ -0,0 +1,13 @@
+// Destructuring
+
+const producto = {
+    nombreProducto: 'Monitor 20 Pulgadas',
+    precio:300,
+    disponible:true
+}
+
+
+const {nombreProducto, precio, disponible} = producto
+
+console.log(precio);
+console.log(disponible);
