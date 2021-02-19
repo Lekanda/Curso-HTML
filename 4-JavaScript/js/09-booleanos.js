a3fb72ddd6f28d57f4b4ce77a5656aeabe685985
lekanda <abernaolao@gmail.com>
2021-02-19 09:57:18 +0100

    106.Booleanos.

---
+++ 4-JavaScript/js/09-booleanos.js
@@ -0,0 +1,13 @@
+// Booleanos (True, False)
+
+
+const boolean1= true;
+const boolean2=false;
+
+console.log(boolean1);
+console.log(boolean2);
+
+// Booleano pero de tipo Objeto.
+const boolean3=new Boolean(true);
+
+console.log(boolean3);
