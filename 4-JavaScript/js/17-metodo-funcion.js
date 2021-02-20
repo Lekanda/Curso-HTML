cbbf00fa1e72dc70658e8288afed4f6333165186
lekanda <abernaolao@gmail.com>
2021-02-20 15:28:42 +0100

    117.Metodos y funciones.

---
+++ 4-JavaScript/js/17-metodo-funcion.js
@@ -0,0 +1,18 @@
+// Diferencia entre Metodo y Funcion.
+
+const numero1 =20;
+const numero2 ="20";
+const numero3 ="veinte";
+// ParseInt
+console.log(numero1);
+console.log(numero2);
+// Cambia de STRING a NUMBER.
+console.log(parseInt(numero2));// ParseInt es una Funcion.
+console.log(parseInt(numero3));// Devuelve NaN. No puede convertir.
+
+
+// Metodos. Cuando va una variable.*() .
+console.log(numero1.toString());// Metodo para cambiar a string.
+
+
+
