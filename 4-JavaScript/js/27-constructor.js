64519e02af39a7c3e6afd35c860381a5473590e9
lekanda <abernaolao@gmail.com>
2021-02-23 18:17:22 +0100

    129.Prototypes.

--- 4-JavaScript/js/27-constructor.js
+++ 4-JavaScript/js/27-constructor.js
@@ -8,18 +8,44 @@ const producto = {
 }
 
 /* Object Contructor */
+function Cliente(nombre,apellido){
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+// El PROTOTYPE nos permite crear funciones que estan unidas a un objeto. Y se guardan en su PROTOTYPE.
+Cliente.prototype.formatearCliente = function() {
+    return `SR ${this.nombre}  ${this.apellido} y Usia`;
+}
+
+
+
+
 function Producto(nombre,precio,disponible) {
     this.nombre=nombre;
     this.precio=precio;
     this.disponible=disponible;
-
 }
+Producto.prototype.formatearProducto = function() {
+    return `El producto ${this.nombre} vale ${this.precio}`;
+}
+
+
 const producto2=new Producto('Monitor curvo 49 pulgadas"',800, true);
-const producto3=new Producto('Monitor curvo 4 pulgadas"',90, true);
-const producto4=new Producto('Monitor curvo 45 pulgadas"',30, true);
-const producto5=new Producto('Monitor curvo 48 pulgadas"',844, true);
+const producto3=new Producto('laptop',900, true);
+const cliente = new Cliente('Juan', 'De la Torre');
+
+
 
 console.log(producto2);
 console.log(producto3);
-console.log(producto4);
-console.log(producto5);
+console.log(cliente);
+
+console.log(producto3.formatearProducto());
+
+console.log(cliente.formatearCliente());
+
+
+
+
+
