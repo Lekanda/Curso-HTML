915f4b19776035d1b5aae3d6d6a83227fc792cbb
lekanda <abernaolao@gmail.com>
2021-02-24 19:21:49 +0100

    131.Herencia.

--- 4-JavaScript/js/28-Clases.js
+++ 4-JavaScript/js/28-Clases.js
@@ -1,6 +1,5 @@
 // Clases
 class Producto{
-
     constructor(nombre,precio){
         this.nombre=nombre;
         this.precio=precio;
@@ -9,12 +8,42 @@ class Producto{
     formatearProducto(){
         return `El producto ${this.nombre} vale ${this.precio}`;
     }
+    obtenerPrecio() {
+        console.log(this.precio);
+    }
 }
 const producto = new Producto();
-
 const producto1=new Producto('Monitor curvo 49 pulgadas"',800);
-const producto2=new Producto('laptop',900);
+// console.log(producto1);
+// console.log(producto2);
+console.log(producto1.formatearProducto());
 
-console.log(producto1);
-console.log(producto2);
-console.log(producto2.formatearProducto());
+
+
+
+
+
+
+// Herencia
+class Libro extends Producto{
+    constructor(nombre,precio,isbn){
+        // Con super heredamos del constructor padre.
+        super(nombre,precio);
+        this.isbn=isbn;
+    }
+    formatearProducto(){
+        // Con return va con TEMPLATE STRING
+        return `${super.formatearProducto()} y su isbn es ${this.isbn}`;
+    }
+    obtenerPrecio(){
+        // Con console hay que poner otro.
+        super.obtenerPrecio();
+        console.log('Y si hay en existencia');
+    }
+}
+const libro = new Libro('JS la revolución',120,'64654654646546546');
+console.log(libro.formatearProducto());
+console.log(libro.obtenerPrecio());
+
+
+
