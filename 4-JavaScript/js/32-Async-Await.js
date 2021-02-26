b53256ef7c0acd8ecb571cefafcd21bbae977fce
lekanda <abernaolao@gmail.com>
2021-02-26 14:18:07 +0100

    136. 2 consultas Async Await.

--- 4-JavaScript/js/32-Async-Await.js
+++ 4-JavaScript/js/32-Async-Await.js
@@ -11,17 +11,31 @@ function descargarNuevosClientes() {
     });
 }
 
+function descargarUltimosPedidos() {
+    return new Promise(resolve=>{
+        console.log('Descargando ultimos pedidos....');
+
+        setTimeout(() => {
+            resolve('Los pedidos fueron descargados')
+        }, 3000);
+
+    });
+}
+
 async function app(){
     try {
-        const resultado = await descargarNuevosClientes();
-        console.log(resultado);
+        // const clientes = descargarNuevosClientes();
+        // const pedidos = descargarUltimosPedidos();
+        const resultado = await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()]);
+        console.log(resultado[0]);
+        console.log(resultado[1]);
     } catch (error) {
         console.log(error);
     }
 }
 app();
 
-console.log('Este codigo no se bloquea');
+// console.log('Este codigo no se bloquea');
 
 
 // Temporizador
