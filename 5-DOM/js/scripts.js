0fcdf34d885ee8facc82f29f62a881c569ed93f7
lekanda <abernaolao@gmail.com>
2021-02-27 14:14:54 +0100

    139.QuerySelectorAll. GetElementById.

--- 5-DOM/js/scripts.js
+++ 5-DOM/js/scripts.js
@@ -1,18 +1,30 @@
-// querySelector (MOderna)
+//************* */ querySelector (MOderna) **************
 const heading = document.querySelector('#heading') // Retorna 0 o 1 elemento.
 heading.textContent = 'Nuevo Heading';
 // Añade una clase nueva.
 heading.classList.add('nueva-clase');
-console.log(heading);
+// console.log(heading);
+
+
+//************* */ querySelectorAll (Moderna) **********************
+// MODO 1
+// const enlaces = document.querySelectorAll('.navegacion a');
+// console.log(enlaces[0]);
+// enlaces[0].textContent='Gurea';
+
+// MODO 2 (Con una linea)
+// const enlaces = document.querySelectorAll('.navegacion a')[0].textContent='Gurea';
+
+// MODO 3 (Mas de una linea)
+const enlaces = document.querySelectorAll('.navegacion a')
+enlaces[0].textContent='Gurea';
+// enlaces[0].href='http://www.google.com';
+enlaces[0].classList.add('nueva-clase2');
+enlaces[0].classList.remove('nueva-clase2');
+// console.log(enlaces[0]);
 
 
 
-
-// querySelectorAll (MOderna)
-
-
-
-
-
-
-// getElementById (Vieja pero muy usada)
+//************** */ getElementById (Vieja pero muy usada) **********
+const heading2 = document.getElementById('heading');
+console.log(heading2);
