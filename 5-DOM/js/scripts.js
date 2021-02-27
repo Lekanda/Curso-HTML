7763c6c45dfd3963e106bfe08ffb94c349c81afc
lekanda <abernaolao@gmail.com>
2021-02-27 15:06:49 +0100

    140.createElement.

--- 5-DOM/js/scripts.js
+++ 5-DOM/js/scripts.js
@@ -1,30 +1,51 @@
-//************* */ querySelector (MOderna) **************
+//************* */ querySelector (Moderna) **************
 const heading = document.querySelector('#heading') // Retorna 0 o 1 elemento.
 heading.textContent = 'Nuevo Heading';
 // Añade una clase nueva.
 heading.classList.add('nueva-clase');
-// console.log(heading);
+console.log(heading);
+
 
 
 //************* */ querySelectorAll (Moderna) **********************
-// MODO 1
+// ***MODO 1
 // const enlaces = document.querySelectorAll('.navegacion a');
 // console.log(enlaces[0]);
 // enlaces[0].textContent='Gurea';
 
-// MODO 2 (Con una linea)
+// ***MODO 2 (Con una linea)
 // const enlaces = document.querySelectorAll('.navegacion a')[0].textContent='Gurea';
 
-// MODO 3 (Mas de una linea)
+// ***MODO 3 (Mas de una linea)
 const enlaces = document.querySelectorAll('.navegacion a')
 enlaces[0].textContent='Gurea';
-// enlaces[0].href='http://www.google.com';
+enlaces[0].href='http://www.google.com';
 enlaces[0].classList.add('nueva-clase2');
 enlaces[0].classList.remove('nueva-clase2');
-// console.log(enlaces[0]);
+console.log(enlaces[0]);
 
 
 
 //************** */ getElementById (Vieja pero muy usada) **********
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+
+/****************** createElement ********************/
+
+const nuevoEnlace = document.createElement('A')
+
+// Agregar HREF
+nuevoEnlace.href = 'nuevo-enlace.html';
+// Agregar el TXT
+nuevoEnlace.textContent = 'Nuevo A';
+// Agregar clases
+nuevoEnlace.classList.add('navegacion__enlace');
+
+// Agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace)
+
+console.log(nuevoEnlace);
+
+
