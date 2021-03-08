85bfcb4192e1a3f4bd7e767eeedd388efdb68653
lekanda <abernaolao@gmail.com>
2021-03-08 15:30:12 +0100

    173.Imagen mas grande al hacer click.

--- 7-Festival-Musica/src/js/galeria.js
+++ 7-Festival-Musica/src/js/galeria.js
@@ -9,8 +9,13 @@ function crearGaleria() {
 
         const imagen = document.createElement('IMG');
         imagen.src = `build/img/thumb/${i}.webp`;
-
+        // atributo personalizado(data-imagen-id)
+        imagen.dataset.imagenId = i;
         // console.log(imagen);
+
+        // Añadir la funcion de mostrarImagen()
+        imagen.onclick = mostrarImagen;
+
 
         const lista = document.createElement('LI');
         lista.appendChild(imagen);
@@ -19,3 +24,59 @@ function crearGaleria() {
 
     }
 }
+
+function mostrarImagen(e) {
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.dataset);
+    // console.log(e.target.dataset.imagenId);
+    // console.log(typeof e.target.dataset.imagenId); // String
+    
+    const id = parseInt(e.target.dataset.imagenId);
+    
+    // console.log(id);
+    // console.log(typeof id);
+
+
+    // Genera la imagen
+    const imagen = document.createElement('IMG');
+    // Ubicacion de imagen
+    imagen.src = `build/img/grande/${id}.webp`;
+    // console.log(imagen);
+
+    // Efecto de imagen grande
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+
+    // cuando se hace click fuera de la imagen se cierra
+    overlay.onclick = function () {
+        overlay.remove();
+    }
+
+    // Boton para cerrar la imagen
+    const cerrarImagen = document.createElement('P');
+    cerrarImagen.textContent = 'X';
+    cerrarImagen.classList.add('btn-cerrar');
+    // Cuando se presiona se cierra la imagen
+    cerrarImagen.onclick = function () {
+        overlay.remove();
+    }
+
+
+
+
+    overlay.appendChild(cerrarImagen)
+
+
+
+    // Mostrar en el HTMl
+    const  body = document.querySelector('body');
+    body.appendChild(overlay)
+    body.classList.add('fijar-body');
+
+}
+
+
+
