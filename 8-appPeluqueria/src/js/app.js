9f2f8a1cb6416ae3d8450cbf5e1a60b1ad348647
lekanda <abernaolao@gmail.com>
2021-03-12 18:52:59 +0100

    188.Seleccionar servicios.

--- 8-appPeluqueria/src/js/app.js
+++ 8-appPeluqueria/src/js/app.js
@@ -37,8 +37,16 @@ async function mostrarServicios() {
             precioServicio.classList.add('precio-servicio');
             // console.log(precioServicio);
 
+            // Generar div contenedor de servicio
             const servicioDiv = document.createElement('DIV');
             servicioDiv.classList.add('servicio');
+            servicioDiv.dataset.idServicio = id;
+
+
+            // Selecciona un servicio para la cita
+            servicioDiv.onclick = seleccionarServicio;
+
+
 
             // Inyectar precio y nombre al Div
             servicioDiv.appendChild(nombreServicio);
@@ -55,4 +63,26 @@ async function mostrarServicios() {
 }
 
 
+function seleccionarServicio(e) {
+    // console.log(e.target.tagName);
 
+    // Como inicalizamos sin valor LET.
+    let elemento;
+    // Forzar que el elemento al cual le damos click sea el div.
+    if (e.target.tagName === 'P'){
+        elemento = e.target.parentElement;
+    } else {
+        // console.log('click en el DIV');
+        elemento = e.target
+    }
+    console.log(elemento.dataset.idServicio);
+
+
+    if (elemento.classList.contains('seleccionado')) {
+        elemento.classList.remove('seleccionado');
+    } else {
+        elemento.classList.add('seleccionado');
+    }
+
+    
+}
