29b1f0ab0f031d27850edd812378df85b10f2c74
lekanda <abernaolao@gmail.com>
2021-03-12 17:20:57 +0100

    186.Creando el HTML de los servicios.

--- 8-appPeluqueria/src/js/app.js
+++ 8-appPeluqueria/src/js/app.js
@@ -9,7 +9,6 @@ function iniciarApp() {
 
 async function mostrarServicios() {
     // console.log('Consultando la DB');
-
     try {
         const resultado = await fetch('./servicios.json');
         const db = await resultado.json();
@@ -21,16 +20,39 @@ async function mostrarServicios() {
         // console.log(servicios);
         // console.log(servicios);
 
-
         // Generar el HTML
         servicios.forEach(servicio => {
             // console.log(servicio);
             const {id,nombre,precio} = servicio;
+
             // DOM Scripting
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent= nombre;
+            nombreServicio.classList.add('nombre-servicio');
+            // console.log(nombreServicio);
+
+            const precioServicio = document.createElement('P');
+            // precioServicio.textContent= '$' + ' ' + precio;
+            precioServicio.textContent= `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+            // console.log(precioServicio);
+
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+
+            // Inyectar precio y nombre al Div
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            // Inyectar al HTML
+            document.querySelector('#servicios').appendChild(servicioDiv);
+
 
         });
-
     } catch (error) {
         console.log(error);
     }
 }
+
+
+
