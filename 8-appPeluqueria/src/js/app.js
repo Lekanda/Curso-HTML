cee912c867c0fd7f2a04da25e63ac27f7b24c4e9
lekanda <abernaolao@gmail.com>
2021-03-13 22:11:32 +0100

    189.Trabajando la navegacion por tabs.

--- 8-appPeluqueria/src/js/app.js
+++ 8-appPeluqueria/src/js/app.js
@@ -1,3 +1,5 @@
+let pagina = 1;
+
 document.addEventListener('DOMContentLoaded', () => {
     iniciarApp();
 });
@@ -5,7 +7,64 @@ document.addEventListener('DOMContentLoaded', () => {
 function iniciarApp() {
     // console.log('Iniciando app');
     mostrarServicios();
+
+    // Resalta el div actual segun el tab al que se presiona.
+    mostrarSeccion();
+
+    // Oculta o muestra una seccion segun el tab al que se presiona
+    cambiarSeccion();
+
 }
+
+
+function mostrarSeccion() {
+    const seccionActual = document.querySelector(`#paso-${pagina}`);
+    seccionActual.classList.add('mostrar-seccion');
+
+    // Resalta el Tab actual
+    const tab = document.querySelector(`[data-paso="${pagina}"]`);
+    tab.classList.add('actual');
+}
+
+
+
+
+
+function cambiarSeccion() {
+    const enlaces = document.querySelectorAll('.tabs button')
+
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', e => {
+            e.preventDefault();
+            // console.log('Click en un boton...');
+            // console.log(e.target.dataset);
+            // console.log(e.target.dataset.paso);
+
+
+            // Lo coje del data-set del HTML
+            pagina = parseInt(e.target.dataset.paso);
+            // console.log(pagina);
+
+            // Eliminar .mostrar-seccion de la seccion anterior
+            document.querySelector('.mostrar-seccion').classList.remove('mostrar-seccion');
+
+            // Agrega .mostrar-seccion donde hicimos click
+            const seccion = document.querySelector(`#paso-${pagina}`);
+            seccion.classList.add('mostrar-seccion');
+
+            // Elimina la clase .actual en el tab anterior
+            document.querySelector('.tabs button.actual').classList.remove('actual');
+
+            // Agregar la clase de actual en el nuevo tab
+            const tab = document.querySelector(`[data-paso="${pagina}"]`);
+            tab.classList.add('actual');
+
+        })
+    });
+}
+
+
+
 
 async function mostrarServicios() {
     // console.log('Consultando la DB');
@@ -68,7 +127,7 @@ function seleccionarServicio(e) {
 
     // Como inicalizamos sin valor LET.
     let elemento;
-    // Forzar que el elemento al cual le damos click sea el div.
+    // Forzar que el elemento al cual le damos click sea el div que es donde esta el id.
     if (e.target.tagName === 'P'){
         elemento = e.target.parentElement;
     } else {
@@ -77,12 +136,9 @@ function seleccionarServicio(e) {
     }
     console.log(elemento.dataset.idServicio);
 
-
     if (elemento.classList.contains('seleccionado')) {
         elemento.classList.remove('seleccionado');
     } else {
         elemento.classList.add('seleccionado');
     }
-
-    
 }
