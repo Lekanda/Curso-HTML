07f4391aaf2563e3bb1ae95d742954a43791e469
lekanda <abernaolao@gmail.com>
2021-03-20 13:02:43 +0100

    238.Variables y constantes.

--- IntroduccionPHP_inicio/02-variables.php
+++ IntroduccionPHP_inicio/02-variables.php
@@ -1,5 +1,22 @@
 <?php include 'includes/header.php';
+// Variables
+$nombre = 'Andres';
+// GuionBajo es valido al inicio de la var.
+$_nombre = 'Andres';
+echo $nombre;
+// Indica el tipo y longitud
+var_dump($nombre);
 
+echo '<br>';
+
+// Constantes
+define('constante', 'Valor de constante');
+echo constante;
+
+echo '<br>';
+
+const constante2 = 'Valor de constante 2';
+echo constante2;
 
 
 
