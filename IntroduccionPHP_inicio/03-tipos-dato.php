f3b91b835c50ae07208dbae22e32fa6b2603977e
lekanda <abernaolao@gmail.com>
2021-03-20 13:54:36 +0100

    239.Tipos de Datos en PHP.

--- IntroduccionPHP_inicio/03-tipos-dato.php
+++ IntroduccionPHP_inicio/03-tipos-dato.php
@@ -1,6 +1,42 @@
 <?php include 'includes/header.php';
 
+// Booleano
+$logueado = true;
 
+var_dump($logueado);
+echo'<br/>';
+echo($logueado);
+
+echo'<br/>';
+
+// Enteros
+$numero = 200;
+var_dump($numero);
+echo'<br/>';
+echo($numero);
+
+echo'<br/>';
+
+// Float (Decimales)
+$float = 200.5;
+var_dump($float);
+echo'<br/>';
+echo($float);
+
+echo'<br/>';
+
+// Strings
+$string = 'Esto es una cadena de txt.';
+var_dump($string);
+echo'<br/>';
+echo($string);
+
+echo'<br/>';
+
+// Arreglo-Array
+$array = [1,2,3,4,5,6];
+echo'<br/>';
+var_dump($array);
 
 
 include 'includes/footer.php';
