8c4d17010d445851977b692a750816fa64b0d07b
lekanda <abernaolao@gmail.com>
2021-03-20 18:54:17 +0100

    244.Arreglos Indexados y Funciones para Arreglos.

--- IntroduccionPHP_inicio/08-arrays.php
+++ IntroduccionPHP_inicio/08-arrays.php
@@ -1,4 +1,48 @@
 <?php include 'includes/header.php';
+// Modo 1
+$carrito = ['Tablet','Movil','Desktop'];
+
+echo '<br/>';
+// UTIL PARA VER LOS CONTENIDOS DE UN ARRAY.
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+echo '<br/>';
+
+
+// Acceder a un elemento del array
+echo $carrito[1];
+echo '<br/>';
+$carrito[3] = 'PC';
+echo $carrito[3];
+echo '<br/>';
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+echo '<br/>';
+
+
+// Añade al final del Arreglo.
+array_push($carrito, 'Auriculares');
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+
+
+
+// Añade al inicio del Arreglo.
+array_unshift($carrito, 'Web Cam');
+echo '<pre>';
+var_dump($carrito);
+echo '</pre>';
+
+
+
+// Modo 2
+$carrito2 = array('Cliente1', 'Cliente2','Cliente3');
+echo '<pre>';
+var_dump($carrito2);
+echo '</pre>';
 
 
 
