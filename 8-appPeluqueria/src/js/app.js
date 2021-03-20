84a5371e463b863c8cb030748f6573735e52f8fd
lekanda <abernaolao@gmail.com>
2021-03-20 12:06:14 +0100

    237.Hola Mundo y Extensiones para VS.

--- 8-appPeluqueria/src/js/app.js
+++ 8-appPeluqueria/src/js/app.js
@@ -535,3 +535,4 @@ function horaCita() {
         }
     });
 }
+
---
+++ IntroduccionPHP_inicio/01-hola-mundo.php
@@ -0,0 +1,28 @@
+<?php include 'includes/header.php'; ?>
+
+<?php
+
+echo "Hola Mundo Desde PHPServer";
+
+echo '<br>';
+
+echo("Hola Mundo desde funcion");
+
+echo '<br>';
+
+print('Hola Mundo2');
+
+echo '<br>';
+
+print 'Hola Mundo3';
+
+echo '<br>';
+
+print_r('Hola Mundo 4');
+
+echo '<br>';
+
+var_dump('Hola Mundo 5');
+
+
+include 'includes/footer.php'; ?>
