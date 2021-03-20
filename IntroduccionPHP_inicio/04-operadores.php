97da4fea5fcf367222c802ad5bbced58609e0bf7
lekanda <abernaolao@gmail.com>
2021-03-20 14:06:27 +0100

    241.Operadores en PHP.

--- IntroduccionPHP_inicio/04-operadores.php
+++ IntroduccionPHP_inicio/04-operadores.php
@@ -1,6 +1,15 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
+
+// Operadores ( + - * /)
+echo $numero1 * $numero2;
 
 
+echo '<br/>';
+
+// Multiplica n veces
+echo 2**3; // 3 veces * 2
 
 include 'includes/footer.php';
