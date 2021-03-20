cd10e14509ba567ad10622e8b447740e965450fe
lekanda <abernaolao@gmail.com>
2021-03-20 16:21:15 +0100

    242.Incrementos en PHP.

--- IntroduccionPHP_inicio/06-inc-dec.php
+++ IntroduccionPHP_inicio/06-inc-dec.php
@@ -1,6 +1,25 @@
 <?php include 'includes/header.php';
 
-
-
+// Igual con -
+$numero1 = 30;
+$numero1++;
+// 31
+echo $numero1;
+echo '<br/>';
+// 31.lo imprime antes de sumar.
+echo $numero1++;
+echo '<br/>';
+// 32
+echo $numero1;
+echo '<br/>';
+// 33
+echo ++$numero1;
+echo '<br/>';
+// 33
+echo $numero1;
+$numero1+=5; // Añade 5
+// 38
+echo '<br/>';
+echo $numero1;
 
 include 'includes/footer.php';
