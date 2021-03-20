e35a6ed9cb45b007d10014a7f1b3e567b7fdebcf
lekanda <abernaolao@gmail.com>
2021-03-20 19:20:48 +0100

    245.Arreglos asociativos.

--- IntroduccionPHP_inicio/09-arrays-assoc.php
+++ IntroduccionPHP_inicio/09-arrays-assoc.php
@@ -1,5 +1,40 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium',
+        'disponible' => '100'
+    ]
+];
+
+echo '<pre>';
+var_dump($cliente);
+echo '</pre>';
+echo '</br>';
+
+
+var_dump($cliente['nombre']);
+echo '</br>';
+echo($cliente['nombre']);
+echo '</br>';
+echo($cliente['saldo']);
+echo '</br>';
+
+
+// echo($cliente['informacion']['tipo']);
+echo '</br>';
+// echo($cliente['informacion']['disponible']);
+
+
+// Añadir mas propiedades al arreglo creado.
+$cliente['codigo'] = 1215465454654;
+
+echo '<pre>';
+var_dump($cliente);
+echo '<pre>';
+
 
 
 
