5378cd08cd3e11dedcddff91f4aee593cdafd415
lekanda <abernaolao@gmail.com>
2021-03-21 17:24:08 +0100

    251.PHP en HTML.

--- IntroduccionPHP_inicio/14-foreach.php
+++ IntroduccionPHP_inicio/14-foreach.php
@@ -1,6 +1,34 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'PC',
+        'precio' => 300,
+        'disponible' => false
+    ],
+    [
+        'nombre' => 'Desktop',
+        'precio' => 400,
+        'disponible' => true
+    ]
+];
 
+foreach ($productos as $producto) { ?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre'];?>  </p>
+        <p>Precio: <?php echo $producto['precio'];?> € </p>
+        <p>
+            <?php echo ($producto['disponible']) ? 'Disponible' :  'No Disponible'; ?> 
+        </p>
+    </li>
+
+    <?php
+}
 
 
 include 'includes/footer.php';
