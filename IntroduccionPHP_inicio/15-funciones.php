165dc8a234e67024ba1cdbdfc28c2504d5fc9143
lekanda <abernaolao@gmail.com>
2021-03-22 08:59:53 +0100

    252.Funciones en PHP.

--- IntroduccionPHP_inicio/15-funciones.php
+++ IntroduccionPHP_inicio/15-funciones.php
@@ -1,7 +1,22 @@
-<?php include 'includes/header.php';
+<?php 
+// declare: hace que los tipos de var sean int a la fuerza. Es para que no se pueda poner mas que un int.
+declare(strict_types=1);
+include 'includes/header.php';
 
 
 
+function sumar(int $num1=0,int $num2=0){
+    echo $num1+$num2;
+}
 
+sumar(5,5);
+echo '<br>';
+sumar(15,75);
+echo '<br>';
+sumar(75);
+echo '<br>';
+sumar();
+echo '<br>';
+sumar(10,5);
 
 include 'includes/footer.php';
