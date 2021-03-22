3cd3dc8c07945d169c000be46aa60ed324d554cc
lekanda <abernaolao@gmail.com>
2021-03-22 09:29:51 +0100

    253.Funciones que retornan valores.

--- IntroduccionPHP_inicio/16-types.php
+++ IntroduccionPHP_inicio/16-types.php
@@ -1,4 +1,29 @@
-<?php include 'includes/header.php';
+<?php 
+// Tipos de variable estrictos(true)
+declare(strict_types=1);
+include 'includes/header.php';
+
+// :void - quiere decir que la funcion no retorna nada.(Imprime y listo.)
+// :string - quiere decir que la funcion retorna un string. Hace falta ponerle un  RETURN dentro de la funcion. Igual con FLOAT,ARRAY,INT,....
+// :?string - Opcional puede ser una STRING.
+// :string|int - O un STRING o un INT.
+function usuarioAutenticado(bool $autenticado) : string|int {
+    if ($autenticado) {
+        return "El Usuario esta Autenticado";
+    } else {
+        return 20;
+    }
+}
+
+$usuario = usuarioAutenticado(false);
+echo $usuario;
+
+
+
+
+
+
+
 
 
 
