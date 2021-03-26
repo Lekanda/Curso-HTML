e299347ed50a7f506897852ade88736435bf6239
lekanda <abernaolao@gmail.com>
2021-03-26 14:24:24 +0100

    283.Ajustes a Dark Mode.

--- bienesraices_inicio/src/js/app.js
+++ bienesraices_inicio/src/js/app.js
@@ -5,12 +5,72 @@ document.addEventListener('DOMContentLoaded', function(){
 
 });
 
-function darkMode() {
+
+
+
+
+
+
+// Modo 1 (Curso)
+// function darkMode() {
+//     const botonDarkMode = document.querySelector('.dark-mode-boton');
+//     botonDarkMode.addEventListener('click', function () {
+//         document.body.classList.toggle('dark-mode');
+//     })
+// }
+
+
+// Modo 2 (Guarda los cambios)
+function darkMode(){
+ 
+        
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme-dark)');
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+        
+    prefiereDarkMode.addEventListener('change', function() {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+ 
+    //console.log(prefiereDarkMode.matches);
+        //Boton DarkMode
     const botonDarkMode = document.querySelector('.dark-mode-boton');
-    botonDarkMode.addEventListener('click', function () {
-        document.body.classList.toggle('dark-mode');
-    })
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode'); 
+ 
+        //Para que el modo elegido se quede guardado en local-storage
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('modo-oscuro','true');
+        } else {
+            localStorage.setItem('modo-oscuro','false');
+        }
+    });
+    //Obtenemos el modo del color actual
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
