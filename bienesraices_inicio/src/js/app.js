a3a34f370013c812b3820851da4fcad35fffcb96
lekanda <abernaolao@gmail.com>
2021-03-26 15:19:29 +0100

    284.Preferencias del sistema.(Tema Oscuro).

--- bienesraices_inicio/src/js/app.js
+++ bienesraices_inicio/src/js/app.js
@@ -21,57 +21,31 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 // Modo 2 (Guarda los cambios)
-function darkMode(){
- 
-        
-    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme-dark)');
-    if (prefiereDarkMode.matches) {
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // console.log(prefiereDarkMode.matches);
+
+    if(prefiereDarkMode.matches) {
         document.body.classList.add('dark-mode');
     } else {
         document.body.classList.remove('dark-mode');
     }
-        
+
     prefiereDarkMode.addEventListener('change', function() {
-        if (prefiereDarkMode.matches) {
+        if(prefiereDarkMode.matches) {
             document.body.classList.add('dark-mode');
         } else {
             document.body.classList.remove('dark-mode');
         }
     });
- 
-    //console.log(prefiereDarkMode.matches);
-        //Boton DarkMode
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
-    botonDarkMode.addEventListener('click', function(){
-        document.body.classList.toggle('dark-mode'); 
- 
-        //Para que el modo elegido se quede guardado en local-storage
-        if (document.body.classList.contains('dark-mode')) {
-            localStorage.setItem('modo-oscuro','true');
-        } else {
-            localStorage.setItem('modo-oscuro','false');
-        }
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
     });
-    //Obtenemos el modo del color actual
-    if (localStorage.getItem('modo-oscuro') === 'true') {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
