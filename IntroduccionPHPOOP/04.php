3a9eff531a33d1272bc9a4e1f1b7fadd82398809
lekanda <abernaolao@gmail.com>
2021-04-04 16:21:12 +0200

    342.Herencia de Clases.

--- IntroduccionPHPOOP/04.php
+++ IntroduccionPHPOOP/04.php
@@ -1,4 +1,44 @@
 <?php include 'includes/header.php';
 
+class Transporte{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+    
+    public function getInfo():string{
+        return "El transporte tiene: " . $this->ruedas . " ruedas, y una capacidad de " . $this->capacidad . " persona/s";
+    }
+    
+    public function getRuedas(){
+        return $this->ruedas;
+    }
+}
+
+class Bicicleta extends Transporte{
+    
+}
+
+class Automovil extends Transporte{
+    public function __construct(protected int $ruedas, protected int $capacidad,protected string $transmision)
+    {
+
+    }
+    public function getInfo():string{
+        return "El transporte tiene: " . $this->ruedas . " ruedas, y una capacidad de " . $this->capacidad . " persona/s y una transmision " . $this->transmision;
+    }
+}
+
+$bicicleta = new Bicicleta(2,1);
+echo $bicicleta->getInfo();
+echo "<hr>";
+
+$automovil = new Automovil(4,5,'Automatica');
+echo $automovil->getInfo();
+echo "<hr>";
+
+echo $bicicleta->getRuedas();
+echo "<hr>";
+echo $automovil->getRuedas();
 
 include 'includes/footer.php';
