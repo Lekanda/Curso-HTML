c067dc81a54d37a5ae7e6457341dd7b179183c60
lekanda <abernaolao@gmail.com>
2021-04-04 09:18:19 +0200

    337.Atributos y Constructor en Clases.PHP.

--- IntroduccionPHPOOP/01.php
+++ IntroduccionPHPOOP/01.php
@@ -1,25 +1,41 @@
-<?php include 'includes/header.php';
+<?php
+    declare(strict_types = 1);
+    include 'includes/header.php';
+
+
+
     // Crear una clase
     class Producto {
         public $nombre;
         public $precio;
         public $disponible;
+
+        public function __construct(string $nombre, int $precio, bool $disponible)
+        {
+            $this->nombre = $nombre;
+            $this->precio = $precio;
+            $this->disponible = $disponible;
+        }
     }
-    $producto = new Producto();
-    $producto->nombre = 'Tablet';
-    $producto->precio = 100;
-    $producto->disponible = true;
-    echo ($producto->nombre);
-    // echo "<pre>";
-    // var_dump($producto);
-    // echo "</pre>";
-    
-    $producto2 = new Producto();
-    $producto2->nombre = 'Desktop PC';
-    $producto2->precio = 1000;
-    $producto2->disponible = false;
+
+    $producto = new Producto('Tablet',100, true);
+    $producto3 = new Producto('Monitor',200, true);
+
     echo "<pre>";
-    var_dump($producto2);
+    var_dump($producto);
     echo "</pre>";
+
+    echo "<pre>";
+    var_dump($producto3);
+    echo "</pre>";
+    
+    // $producto2 = new Producto();
+    // $producto2->nombre = 'Desktop PC';
+    // $producto2->precio = 1000;
+    // $producto2->disponible = false;
+    // echo ($producto2->nombre);
+    // echo "<pre>";
+    // var_dump($producto2);
+    // echo "</pre>";
 
 include 'includes/footer.php';
