408ae38370c6ff688fc137500d04c75532a16551
lekanda <abernaolao@gmail.com>
2021-04-05 17:33:43 +0200

    345.Polimorfismo.

--- IntroduccionPHPOOP/07.php
+++ IntroduccionPHPOOP/07.php
@@ -1,4 +1,54 @@
 <?php include 'includes/header.php';
 
+interface TransporteInterfaz{
+    public function getInfo():string;
+    public function getRuedas():int;
+    
+} 
+
+class Transporte implements TransporteInterfaz{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+        
+    }
+    
+    public function getInfo():string{
+        return "El transporte tiene: " . $this->ruedas . " ruedas, y una capacidad de " . $this->capacidad . " persona/s";
+    }
+    
+    public function getRuedas():int{
+        return $this->ruedas;
+    }
+}
+
+
+class Automovil extends Transporte implements TransporteInterfaz{
+    public function __construct(protected int $ruedas, protected int $capacidad,protected string $color)
+    {
+        
+    }
+    public function getInfo():string{
+        return "El transporte AUTO tiene: " . $this->ruedas . " ruedas, y una capacidad de " . $this->capacidad . " persona/s y una color " . $this->color;
+    }
+    public function getColor():string{
+        return "El color es " . $this->color;
+    }
+}
+
+echo "<pre>";
+var_dump($transporte = new Transporte(8,20));
+var_dump($auto = new Automovil(4,4,'rojo'));
+echo "<hr>";
+echo $transporte->getInfo();
+echo "<br>";
+echo $auto->getInfo();
+echo "<br>";
+echo $auto->getColor();
+
+echo "</pre>";
+
+
+
+
 
 include 'includes/footer.php';
