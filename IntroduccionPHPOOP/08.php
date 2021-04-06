bba810f92163f566c6ce2bce98b82a50d57ee771
lekanda <abernaolao@gmail.com>
2021-04-06 10:45:13 +0200

    347.Namespace.

--- IntroduccionPHPOOP/08.php
+++ IntroduccionPHPOOP/08.php
@@ -1,11 +1,18 @@
 <?php include 'includes/header.php';
 
-function mi_autoload ($clase){
-    require __DIR__ . '/clases/' . $clase . '.php';
-}
+use App\Clientes;
+use App\Detalles;
 
+// Incluir las otras clases
+function mi_autoload ($clase){
+    // echo $clase;
+    $partes = explode('\\', $clase);
+    // var_dump($partes[1]);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
+}
 // Manda llamar a las clases segun las necesita.
 spl_autoload_register('mi_autoload');
+
 
 $detalles = new Detalles();
 $clientes = new Clientes();
