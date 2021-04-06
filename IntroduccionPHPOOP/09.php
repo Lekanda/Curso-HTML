9de808ea5ccc4b33ae3e64e7db065201ca03e98f
lekanda <abernaolao@gmail.com>
2021-04-06 18:06:31 +0200

    352.Consultar DB con Myqli en POO PHP.

--- IntroduccionPHPOOP/09.php
+++ IntroduccionPHPOOP/09.php
@@ -1,4 +1,41 @@
 <?php include 'includes/header.php';
 
+// Conectar a la DB con MySqli
+$db = new mysqli('localhost','root','root','bienesraices');
+// var_dump($db);
+
+// Crear el query
+$query = "SELECT titulo,imagen FROM propiedades";
+
+//Preparamos
+$stmt = $db->prepare($query);
+
+//Ejecutamos
+$stmt->execute();
+
+// Creamos la variable
+$stmt->bind_result($titulo,$imagen);
+
+// Asignamos el resultado
+// $stmt->fetch();
+
+
+
+while ($stmt->fetch()):
+    var_dump($titulo);
+    var_dump($imagen);
+endwhile;
+
+
+// Imprimir el resultado
+// var_dump($titulo);
+// var_dump($imagen);
+
+
+
+// while ($row = $resultado->fetch_assoc()):
+//     var_dump($row);
+// endwhile;
+
 
 include 'includes/footer.php';
--- IntroduccionPHPOOP/vendor/composer/autoload_real.php
+++ IntroduccionPHPOOP/vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitc0cee635fc1c0a68e6f9746fcea8386c
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitc0cee635fc1c0a68e6f9746fcea8386c', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitc0cee635fc1c0a68e6f9746fcea8386c', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
